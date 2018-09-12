d3163757535b074ba10cddf4d0aaba2080137e33
Esteban Zignego <esteban.zignego@gmail.com>
2018-09-12 16:47:38 -0300

    add post model

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ActiveRecord::Base
+  
+  enum state: {
+    in_progress: 0,   # --> lo puede editar el autor
+    canceled: 1,      # --> lo pueden cancelar todos
+    censored: 2,      # --> solo se puede censurar por el editor
+    in_revision: 3,   # --> solo lo puede aceptar/editar el editor
+    published: 4      # --> solo lo puede editar el editor
+  }
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180912192239) do
+ActiveRecord::Schema.define(version: 20180912194525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 20180912192239) do
     t.string   "description", default: "",  null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      default: "", null: false
+    t.integer  "state",      default: 0,  null: false
+    t.string   "summary"
+    t.text     "body",                    null: false
+    t.string   "author"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "reservations", force: :cascade do |t|
