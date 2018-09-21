3c4d1c1b4453287a6937246fbb20c5521fa7e5ad
agustinwalas <agustinwalas@gmail.com>
2018-09-21 15:31:19 -0300

    add validation authorizeds

--- app/models/authorized.rb
+++ app/models/authorized.rb
@@ -1,3 +1,6 @@
 class Authorized < ActiveRecord::Base
   belongs_to :user
+  validates :name,:telephone, :dni,:date_in,:user, presence: true
+  validates :dni, length: { in: 7..8 }
+  validates :dni,:telephone, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  validates :name, :dni,:telephone, presence: true
+  validates :dni, length: { in: 7..8 }
+  validates :dni, :telephone, numericality: { only_integer: true }
   has_many :reservation
 
   def to_s
