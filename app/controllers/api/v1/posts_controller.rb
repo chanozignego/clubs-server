bcd1eef0a3c840f34ed30b730a4ecd80cb04f370
Esteban Zignego <esteban.zignego@gmail.com>
2018-10-29 12:28:52 -0300

    add show post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -12,6 +12,15 @@ module Api
         super
       end
 
+      api :GET, '/v1/posts/:id', "Return one post"
+      param :id, :number, required: true
+      error :code => 404, :desc => "Page Not Found"
+      error :code => 422, :desc => "Unprocessable Entity"
+      formats ['json']          
+      def show
+        super
+      end
+
     end
   end
 end
